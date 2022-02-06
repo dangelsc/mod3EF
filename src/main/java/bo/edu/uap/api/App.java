ac3233ba9b97452462808181d4eff17feb306373
dangelsc <dangel_sc@hotmail.com>
2022-02-06 07:54:02 -0800

    prueba de spark en direccion http://localhost:4567

--- src/main/java/bo/edu/uap/api/App.java
+++ src/main/java/bo/edu/uap/api/App.java
@@ -1,5 +1,6 @@
 package bo.edu.uap.api;
-
+import static spark.Spark.*;
+import spark.*;
 /**
  * Hello world!
  *
@@ -8,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "******************Iniciando el servidor******************" );
+        get("/entornos",(req,res)->"En el ambiente <ambiente>");
     }
 }
