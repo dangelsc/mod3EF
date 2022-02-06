008e75eb2479cf13e088e96f6f3ccbc266810af8
dangelsc <dangel_sc@hotmail.com>
2022-02-06 08:21:07 -0800

    habilitado las variables de perfiles y corregido la configuracion de pom del directorio

--- src/main/java/bo/edu/uap/api/App.java
+++ src/main/java/bo/edu/uap/api/App.java
@@ -1,6 +1,7 @@
 package bo.edu.uap.api;
 import static spark.Spark.*;
 import spark.*;
+import java.util.Properties;
 /**
  * Hello world!
  *
@@ -9,7 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        Properties properties = new Properties();
+        try{
+            properties.load(App.class.getClassLoader().getResourceAsStream("vars.properties"));
+        }catch(Exception e){
+            System.out.println("Ooops!!! , hay un error, "+e.getMessage());
+            System.exit(0);
+        }
         System.out.println( "******************Iniciando el servidor******************" );
+        System.out.println(properties.getProperty("ambiente"));
         get("/entornos",(req,res)->"En el ambiente <ambiente>");
     }
 }
